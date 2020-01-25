78a34d4b112effa4a76c7e4e98c42f2962f66e51
Mauritz Schild <41117752+mauschil@users.noreply.github.com>
2020-01-25 15:48:16 -0500

    Update mememeister.js

--- mememeister.js
+++ mememeister.js
@@ -8,28 +8,33 @@ const MemeCodes = "odns - one does not simply\n"
                 + "xeverywhere - buzz lightyear everywhere\n"
                 + "pika - surprised pickachu";
 
-// When bot receives message, check that its not from bot and starts with !mememeister.
+// When bot receives message, check if message uses specified commands.
 client.on('message', (receivedMessage) => {
     if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
         return;
     }
 
+    // Check if help command.
     if (receivedMessage.content.startsWith("!MMHelp")) {
         receivedMessage.channel.send("!MMMemeCodes - display a list of memes you can create.\n!MMMeme - create a meme by using a meme " 
         + "code and two text variables. (!MMMeme,memecode,text2,text2)");
 
+      // Check if code code requests.
     } else if (receivedMessage.content.startsWith("!MMMemeCodes")) {
         receivedMessage.channel.send(MemeCodes);
 
+      // Check if meme request.
     } else if (receivedMessage.content.startsWith("!MMMeme")) {
 
-        var fullCommand = receivedMessage.content.substr(13);
-        var splitCommand = fullCommand.split(",");
-        var template = splitCommand[0];
-        var textZero = splitCommand[1];
-        var textOne = splitCommand[2];
+        // Get meme request and split message into single values.
+        var fullCommand = receivedMessage.content.substr(8); // Remove command from front and save string.
+        var splitCommand = fullCommand.split(","); // Split command by commas and save in array.
+        var template = splitCommand[0]; // Index 0 specifies the type meme requested.
+        var textZero = splitCommand[1]; // The first text for the meme.
+        var textOne = splitCommand[2]; // The second text for the meme.
         var tempID = "";
-
+      
+        // Check witch code requested meme is.
         switch (template) {
             case "odns":
                 tempID += "61579";
@@ -56,14 +61,18 @@ client.on('message', (receivedMessage) => {
                 break;
         }
 
+        // Make POST request to imgflip API.
         var request = require('request');
         request.post({
           headers: {'content-type' : 'application/x-www-form-urlencoded'},
           url:     'https://api.imgflip.com/caption_image',
           body: "template_id=" + tempID + "&username=fotogi&password=123456789!Abc&text0=" + textZero + "&text1=" + textOne
         }, function(error, response, body){
+            // Parse response into json.
             var json = JSON.parse(body);
+            // Extract img url from response.
             var memeURL = json.data.url;
+            // Tag user and post link.
             receivedMessage.channel.send(receivedMessage.author.toString() + " here's your spicy meme!\n" + memeURL);
         });        
     }
